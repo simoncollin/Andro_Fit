3187c8b1f23eb681670f862c5fcbd27971f44636
RootzMana <matthieu.desvignes.md@gmail.com>
2019-10-30 17:24:25 +0100

    Bottom bar UI finished Icons added TODO: Event handler in Presenter class

--- app/src/main/java/com/cnamge/fipinfo/androfit/MainActivity.java
+++ app/src/main/java/com/cnamge/fipinfo/androfit/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cnamge.fipinfo.androfit;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
@@ -20,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton bottomBarButton = findViewById(R.id.bottom_bar_button);
         bottomBarButton.setOnClickListener(view -> this.onBottomBarButtonClick());
+
+        ImageButton sessionsButon = findViewById(R.id.bottom_bar_sessions_button);
+        ImageButton mealsButon = findViewById(R.id.bottom_bar_meals_button);
+        ImageButton friendsButon = findViewById(R.id.bottom_bar_friends_button);
+        ImageButton settingsButon = findViewById(R.id.bottom_bar_settings_button);
+
+        sessionsButon.setOnClickListener(view -> Toast.makeText(this, "sessionsButtonClicked", Toast.LENGTH_SHORT).show());
+        mealsButon.setOnClickListener(view -> Toast.makeText(this, "mealsButtonClicked", Toast.LENGTH_SHORT).show());
+        friendsButon.setOnClickListener(view -> Toast.makeText(this, "friendsButtonClicked", Toast.LENGTH_SHORT).show());
+        settingsButon.setOnClickListener(view -> Toast.makeText(this, "settingsButtonClicked", Toast.LENGTH_SHORT).show());
     }
 
     private void onBottomBarButtonClick(){
