7dfe3a08a747b3f51682ca54323179d0738bef27
RootzMana <matthieu.desvignes.md@gmail.com>
2019-10-30 12:15:32 +0100

    Bottom Bar Add bottom bar + Floating action button and setup OnCLickListener

--- app/src/main/java/com/cnamge/fipinfo/androfit/MainActivity.java
+++ app/src/main/java/com/cnamge/fipinfo/androfit/MainActivity.java
@@ -1,9 +1,12 @@
 package com.cnamge.fipinfo.androfit;
 
 import android.os.Bundle;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar appBar = findViewById(R.id.app_bar);
         setActionBar(appBar);
+
+        FloatingActionButton bottomBarButton = findViewById(R.id.bottom_bar_button);
+        bottomBarButton.setOnClickListener(view -> this.onBottomBarButtonClick());
+    }
+
+    private void onBottomBarButtonClick(){
+        Toast.makeText(this, "onBottomBarButtonClick", Toast.LENGTH_SHORT).show();
     }
 }
