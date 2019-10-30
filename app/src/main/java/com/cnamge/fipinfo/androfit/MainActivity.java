c586963f133a9569e552e7b40908c2f063f80e99
Alix <alix@spacefoot.com>
2019-10-30 16:37:28 +0100

    persistence checked

--- app/src/main/java/com/cnamge/fipinfo/androfit/MainActivity.java
+++ app/src/main/java/com/cnamge/fipinfo/androfit/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cnamge.fipinfo.androfit;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
@@ -14,15 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        /*
         Session session = new Session();
         session.setName("Session name");
         session.save();
-
-        Session persisted = Session.findById(Session.class, session.getId());
+        */
+        Session persisted = Session.findById(Session.class, (long) 1);
 
         TextView test = findViewById(R.id.app_bar_rightTextView);
-        test.setText(persisted.getName());
+        test.setText((persisted.getName() + " zbeub"));
+        Log.v("RESULTS", persisted.getName());
 
         Toolbar appBar = findViewById(R.id.app_bar);
         setActionBar(appBar);
