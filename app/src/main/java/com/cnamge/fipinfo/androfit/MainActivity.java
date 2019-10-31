c3fa66cc508eebe3dee27fe457aebd2c30d466fb
RootzMana <matthieu.desvignes.md@gmail.com>
2019-10-31 15:19:37 +0100

    Update architecture in order to respect MVP + upgrade Bottom bar UI

--- app/src/main/java/com/cnamge/fipinfo/androfit/MainActivity.java
+++ app/src/main/java/com/cnamge/fipinfo/androfit/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cnamge.fipinfo.androfit;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
@@ -9,7 +11,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainInterface{
+
+    // Bottom bar buttons
+    private FloatingActionButton bottomBarButton;
+    private ImageButton sessionsButton;
+    private ImageButton mealsButton;
+    private ImageButton friendsButton;
+    private ImageButton settingsButton;
+    private TextView appBarRightText;
+    MainPresenter mainPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,21 +30,68 @@ public class MainActivity extends AppCompatActivity {
         Toolbar appBar = findViewById(R.id.app_bar);
         setActionBar(appBar);
 
-        FloatingActionButton bottomBarButton = findViewById(R.id.bottom_bar_button);
-        bottomBarButton.setOnClickListener(view -> this.onBottomBarButtonClick());
-
-        ImageButton sessionsButon = findViewById(R.id.bottom_bar_sessions_button);
-        ImageButton mealsButon = findViewById(R.id.bottom_bar_meals_button);
-        ImageButton friendsButon = findViewById(R.id.bottom_bar_friends_button);
-        ImageButton settingsButon = findViewById(R.id.bottom_bar_settings_button);
-
-        sessionsButon.setOnClickListener(view -> Toast.makeText(this, "sessionsButtonClicked", Toast.LENGTH_SHORT).show());
-        mealsButon.setOnClickListener(view -> Toast.makeText(this, "mealsButtonClicked", Toast.LENGTH_SHORT).show());
-        friendsButon.setOnClickListener(view -> Toast.makeText(this, "friendsButtonClicked", Toast.LENGTH_SHORT).show());
-        settingsButon.setOnClickListener(view -> Toast.makeText(this, "settingsButtonClicked", Toast.LENGTH_SHORT).show());
+        this.mainPresenter = new MainPresenter(this);
+        this.setupBottomBar();
     }
 
-    private void onBottomBarButtonClick(){
-        Toast.makeText(this, "onBottomBarButtonClick", Toast.LENGTH_SHORT).show();
+    private void setupBottomBar(){
+        this.bottomBarButton = findViewById(R.id.bottom_bar_button);
+        this.sessionsButton = findViewById(R.id.bottom_bar_sessions_button);
+        this.mealsButton = findViewById(R.id.bottom_bar_meals_button);
+        this.friendsButton = findViewById(R.id.bottom_bar_friends_button);
+        this.settingsButton = findViewById(R.id.bottom_bar_settings_button);
+        this.appBarRightText = findViewById(R.id.app_bar_rightTextView);
+
+        //Attach event listener
+        this.bottomBarButton.setOnClickListener(view -> mainPresenter.onBottomBarAddButtonClick());
+        this.sessionsButton.setOnClickListener(view -> mainPresenter.onBottomBarSessionsButtonClick(view));
+        this.mealsButton.setOnClickListener(view -> mainPresenter.onBottomBarMealsButtonClick(view));
+        this.friendsButton.setOnClickListener(view -> mainPresenter.onBottomBarFriendsButtonClick(view));
+        this.settingsButton.setOnClickListener(view -> mainPresenter.onBottomBarSettingsButtonClick(view));
+
+        this.selectView(this.sessionsButton);
+    }
+
+    // Activity life cycle
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mainPresenter.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mainPresenter.onDestroy();
+        super.onDestroy();
+    }
+
+    // Interface methods
+    @Override
+    public void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void selectView(View view){
+        if (view != null) {
+            this.sessionsButton.setAlpha(0.5f);
+            this.mealsButton.setAlpha(0.5f);
+            this.friendsButton.setAlpha(0.5f);
+            this.settingsButton.setAlpha(0.5f);
+
+            if (view == this.sessionsButton) {
+                this.sessionsButton.setAlpha(1f);
+                this.appBarRightText.setText(R.string.title_sessions);
+            } else if (view == this.mealsButton) {
+                this.mealsButton.setAlpha(1f);
+                this.appBarRightText.setText(R.string.title_meals);
+            } else if (view == this.friendsButton) {
+                this.friendsButton.setAlpha(1f);
+                this.appBarRightText.setText(R.string.title_friends);
+            } else if (view == this.settingsButton) {
+                this.settingsButton.setAlpha(1f);
+                this.appBarRightText.setText(R.string.title_settings);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/cnamge/fipinfo/androfit/MainInterface.java
@@ -0,0 +1,10 @@
+package com.cnamge.fipinfo.androfit;
+
+import android.view.View;
+
+public interface MainInterface {
+
+    void showMessage(String message);
+
+    void selectView(View view);
+}
