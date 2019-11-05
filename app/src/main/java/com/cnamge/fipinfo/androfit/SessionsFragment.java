1c6469822e62ea27518e9f183cf673226fc14b82
RootzMana <matthieu.desvignes.md@gmail.com>
2019-11-05 15:52:29 +0100

    Create new fragment classes

---
+++ app/src/main/java/com/cnamge/fipinfo/androfit/SessionsFragment.java
@@ -0,0 +1,18 @@
+package com.cnamge.fipinfo.androfit;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class SessionsFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.sessions_fragment, container, false);
+
+        return root;
+    }
+}
