4d876715964f23e63c36fc3cac947c105adbcd5f
Matthieu Desvignes <Matthieu.Desvignes@saint-gobain.com>
2019-12-17 17:02:30 +0100

    Friends Add fake data

--- app/src/main/java/com/cnamge/fipinfo/androfit/main/MainActivity.java
+++ app/src/main/java/com/cnamge/fipinfo/androfit/main/MainActivity.java
@@ -17,8 +17,10 @@ import com.cnamge.fipinfo.androfit.R;
 import com.cnamge.fipinfo.androfit.fragments.FriendsFragment;
 import com.cnamge.fipinfo.androfit.fragments.MealsFragment;
 import com.cnamge.fipinfo.androfit.fragments.SettingsFragment;
+import com.cnamge.fipinfo.androfit.model.FriendRequest;
 import com.cnamge.fipinfo.androfit.model.Meal;
 import com.cnamge.fipinfo.androfit.model.Session;
+import com.cnamge.fipinfo.androfit.model.User;
 import com.cnamge.fipinfo.androfit.sessions.sessionEdit.SessionEditActivity;
 import com.cnamge.fipinfo.androfit.sessions.sessionsList.SessionsFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -157,39 +159,78 @@ public class MainActivity extends AppCompatActivity implements MainInterface {
     }
     
     // TODO: Remove after devs
-    protected void purgeAndReplaceFixtures(){
+    protected void purgeAndReplaceFixtures() throws Exception{
         SugarRecord.deleteAll(Meal.class);
+        SugarRecord.deleteAll(User.class);
+        SugarRecord.deleteAll(FriendRequest.class);
+        SugarRecord.deleteAll(Session.class);
 
-        for (int i = 1; i <= 10; i++) {
+        User currentUser = new User("Perlinpinpin");
+        for (int i = 1; i <= 5; i++) {
             String desc;
+            Boolean accepted;
             if (i % 2 == 0) {
                 desc = "description " + i;
+                accepted = true;
             } else {
                 desc = "";
+                accepted = false;
             }
-            (new Meal(
-                    "Meal number " + i,
-                    System.currentTimeMillis() + (i * 6000),
-                    desc,
-                    "Meal url " + i
-            )).save();
+
+            User user = new User("friend n° " + i);
+            user.save();
+
+            (new FriendRequest(currentUser, accepted, user)).save();
+
+            for (int j = 0; j < 3; j++) {
+                (new Meal(
+                        "Meal number " + j,
+                        System.currentTimeMillis() + (j * 6000),
+                        desc,
+                        "Meal url " + j,
+                        user
+                )).save();
+                (new Session(
+                        "Name " + j,
+                        "Location " + j,
+                        System.currentTimeMillis() + (j * 6000),
+                        System.currentTimeMillis() + (j * 66060),
+                        "Description " + j,
+                        0,
+                        user
+                )).save();
+            }
         }
 
         List<Session> sessions = SugarRecord.listAll(Session.class);
         List<Meal> meals = SugarRecord.listAll(Meal.class);
+        List<User> users = SugarRecord.listAll(User.class);
+        List<FriendRequest> friendsRequest = SugarRecord.listAll(FriendRequest.class);
 
         Log.v("purgeAndReplaceFixtures", "Sessions count: " + sessions.size());
         Log.v("purgeAndReplaceFixtures", "Meals count: " + meals.size());
+        Log.v("purgeAndReplaceFixtures", "users count: " + users.size());
+        Log.v("purgeAndReplaceFixtures", "friendsRequest count: " + friendsRequest.size());
 
         Session firstSession = sessions.get(0);
         Meal firstMeal = meals.get(0);
+        User firstUser = users.get(0);
+        FriendRequest firstFriendRequest = friendsRequest.get(0);
+
         Log.v("purgeAndReplaceFixtures", "difference temps : " + firstSession.getDurationString());
         Log.v("purgeAndReplaceFixtures", "First session: " + firstSession);
         Log.v("purgeAndReplaceFixtures", "First meal: " + firstMeal);
+        Log.v("purgeAndReplaceFixtures", "First user: " + firstUser);
+        Log.v("purgeAndReplaceFixtures", "First friend request: " + firstFriendRequest);
 
         Session lastSession = sessions.get(sessions.size() - 1);
         Meal lastMeal = meals.get(meals.size() - 1);
+        User lastUser = users.get(users.size() - 1);
+        FriendRequest lastFriendRequest = friendsRequest.get(friendsRequest.size() - 1);
+
         Log.v("purgeAndReplaceFixtures", "Last session: " + lastSession);
         Log.v("purgeAndReplaceFixtures", "Last meal: " + lastMeal);
+        Log.v("purgeAndReplaceFixtures", "Last user: " + lastUser);
+        Log.v("purgeAndReplaceFixtures", "Last Friend request: " + lastFriendRequest);
     }
 }
