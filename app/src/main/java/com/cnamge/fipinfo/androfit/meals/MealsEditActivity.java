3af772243c046539609b90fa3c66ff550ae0f3db
simoncollin <simon.collin.auditeur@lecnam.net>
2019-12-23 00:16:45 +0100

    test ajout depuis galery

--- app/src/main/java/com/cnamge/fipinfo/androfit/meals/MealsEditActivity.java
+++ app/src/main/java/com/cnamge/fipinfo/androfit/meals/MealsEditActivity.java
@@ -4,11 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
@@ -36,6 +41,9 @@ import java.util.Date;
 import java.util.Locale;
 
 public class MealsEditActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
+
+    private static final int GALLERY_REQUEST_CODE = 1;
+    private ImageView imageView;
 
     public enum CurrentDatePicker {
         MEAL_DATE,
@@ -328,6 +336,48 @@ public class MealsEditActivity extends AppCompatActivity implements TimePickerDi
     }
 
     void onAddMealPictureButtonClick(){
-        showMessage("Add Meal button clicked !");
+
+        pickFromGallery();
+    }
+
+    private void pickFromGallery(){
+        //Create an Intent with action as ACTION_PICK
+        Intent PictureIntent=new Intent(Intent.ACTION_PICK);
+        // Sets the type as image/*. This ensures only components of type image are selected
+        PictureIntent.setType("image/*");
+        //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        PictureIntent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
+        // Launching the Intent
+        startActivityForResult(PictureIntent,GALLERY_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+
+        // Result code is RESULT_OK only if the user selects an Image
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case GALLERY_REQUEST_CODE:
+                    //data.getData return the content URI for the selected Image
+                    Uri selectedImage = data.getData();
+
+                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                    // Get the cursor
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    // Move to first row
+                    cursor.moveToFirst();
+                    //Get the column index of MediaStore.Images.Media.DATA
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    //Gets the String value in the column
+                    String imgDecodableString = cursor.getString(columnIndex);
+
+                    cursor.close();
+                    // Set the Image in ImageView after decoding the String
+                    imageView.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
+
+                    break;
+
+            }
     }
 }
